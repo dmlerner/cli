ff2edd8eb8a57458c6378428ccc8aaf8c9c4dc0c
David Lerner <dmlerner@gmail.com>
2019-11-24 21:18:58 -0800

    .

--- my-awk.py
+++ my-awk.py
@@ -17,35 +17,35 @@ for line in sys.stdin:
 t('lines', lines)
 from_stdin = '\n'.join(lines)
 t('from_stdin', from_stdin)
-time.sleep(.5)
-t('slept')
+#time.sleep(.5)
+#t('slept')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', default='\\t', nargs='?')
-parser.add_argument('-r', default='\\n', nargs='?')
 parser.add_argument('-n', default=1, type=int, nargs='?')
-t('parsing args')
 args = parser.parse_args()
 t(args)
 
-cmd = "awk 'BEGIN{FS=\"%s\"; RS=\"%s\"} {print $%s}'" % tuple(map(str, (args.f, args.r, args.n)))
+cmd = 'cut -c 2-'
 t('cmd=', cmd)
 # print(cmd)
 proc = subprocess.Popen(['/bin/zsh', '-i', '-c', cmd],
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
+                        stderr=sys.stderr,
+                        close_fds=True,
+                        universal_newlines=True,
                         # shell=True,
                         encoding='utf8'
                         )
 t('made proc')
-proc.stdin.write(from_stdin)
-t('wrote')
-stdout = proc.stdout.read()
-t('read', stdout)
-#stdout, stderr = proc.communicate(from_stdin)
-#t('communicated', stdout, stderr, '/communicated')
-#print('??' + stdout + '\nlinetwo')
-print(from_stdin)
-t('stdouted but secretly its just the input i had on purpose...')
-time.sleep(3)
+to_send = from_stdin * args.n
+#print(to_send)
+stdout, stderr = proc.communicate(to_send)
+proc.kill()
+#stdout, stderr = proc.communicate()
+
+t('communicated', 'XXX'+stdout, stderr, '/communicated')
+print(stdout)
+t('printed to my stdout')
+time.sleep(2)
 t('slept, done')
